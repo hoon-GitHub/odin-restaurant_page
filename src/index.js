9ee0d0f4205952d60a3573ebf7f0bf472dcc9999
unknown <hoon81@gmail.com>
2024-05-04 12:56:11 -0400

    Add event listeners for nav buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './style.css';
 
+function renderHome() {
+  console.log("home");
+}
+
+function renderMenu() {
+  console.log("menu");
+}
+
+function renderAbout() {
+  console.log("about");
+}
+
 const home = document.getElementById('home');
 const menu = document.getElementById('menu');
-const contact = document.getElementById('contact');
+const about = document.getElementById('about');
+
+home.addEventListener('click', renderHome);
+menu.addEventListener('click', renderMenu);
+about.addEventListener('click', renderAbout);
